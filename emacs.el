9ffeb8149a31e881ccfdfef9b6adff07e6f30ef7
Sven Welte <sven.welte@mindmatters.de>
2013-12-04 22:14:28 +0100

    switched emacs package repos

--- emacs.el
+++ emacs.el
@@ -1,6 +1,8 @@
+
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -23,7 +25,8 @@
       ac-nrepl
       ace-jump-mode
       rainbow-delimiters
-      nrepl)
+      nrepl
+      soothe-theme)
       "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
