d54622d5838cc724f7f7d772c5f0cdb091f17f22
Sven Welte <sven.welte@mindmatters.de>
2016-10-05 17:56:10 +0200

    tmux: java and clj session no longer needed

--- bin/start_session.sh
+++ bin/start_session.sh
@@ -13,21 +13,4 @@ tmux new-window -t $1:4 -n 'work'
 tmux new-window -t $1:5 -n 'root'
 tmux select-window -t $1:1
 
-
-if [ "$1" = "clj" ]; then
-  tmux send-keys -t $1:1 "emacs -nw" C-m
-  tmux send-keys -t $1:2 "lein repl" C-m
-  tmux send-keys -t $1:3 "bundle exec compass watch" C-m
-  tmux send-keys -t $1:4 "git status" C-m
-  tmux send-keys -t $1:5 "sudo bash" C-m
-fi
-
-if [ "$1" = "java" ]; then
-  tmux send-keys -t $1:1 "vim" C-m
-  #tmux send-keys -t $1:2 "" C-m
-  tmux send-keys -t $1:3 "bundle exec compass watch" C-m
-  tmux send-keys -t $1:4 "git status" C-m
-  tmux send-keys -t $1:5 "sudo bash" C-m
-fi
-
 tmux -2 -u attach-session -t $1
